e3f2483561cf612c1e560eeda445f26e1e454ce7
GorbachevAM <minimiks27@gmail.com>
2020-07-06 16:42:31 +0300

    HW05 base

--- src/test/java/ru/javawebinar/topjava/service/userservice/UserServiceTestCommon.java
+++ src/test/java/ru/javawebinar/topjava/service/userservice/UserServiceTestCommon.java
@@ -1,19 +1,13 @@
-package ru.javawebinar.topjava.service;
+package ru.javawebinar.topjava.service.userservice;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.CacheManager;
 import org.springframework.dao.DataAccessException;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlConfig;
-import org.springframework.test.context.junit4.SpringRunner;
-import ru.javawebinar.topjava.ActiveDbProfileResolver;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.service.ServiceTestCommon;
+import ru.javawebinar.topjava.service.UserService;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.util.List;
@@ -21,24 +15,15 @@ import java.util.List;
 import static org.junit.Assert.assertThrows;
 import static ru.javawebinar.topjava.UserTestData.*;
 
-@ContextConfiguration({
-        "classpath:spring/spring-app.xml",
-        "classpath:spring/spring-db.xml"
-})
-@RunWith(SpringRunner.class)
-@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ActiveProfiles(resolver = ActiveDbProfileResolver.class)
-public class UserServiceTest {
+
+public abstract class UserServiceTestCommon extends ServiceTestCommon {
 
     @Autowired
     private UserService service;
 
-    @Autowired
-    private CacheManager cacheManager;
-
     @Before
     public void setUp() throws Exception {
-        cacheManager.getCache("users").clear();
+        super.cacheManager.getCache("users").clear();
     }
 
     @Test
